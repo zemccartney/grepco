aa837c8c49f7a9878bf750fecc6e009a4684a3a7
Zack McCartney <zemccartney@gmail.com>
2025-11-02 22:50:28 -0500

    formatting fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
 import inoxToolsContentUtils from "@inox-tools/content-utils";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [inoxToolsContentUtils()]
+    integrations: [inoxToolsContentUtils()]
 });
